34380a8a7231a3fedf2f9255eb0c535ab0b938dd
Felix Kariuki <fkariuki1@live.co.uk>
2019-07-28 14:09:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function happyHolidays() {
   return 'Happy holidays!'
 }
 
-function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
+function happyHolidaysTo(Felix) {
+  return 'Happy holidays, ${Felix}!'
 }
 
 function happyHolidaysTo(MerryChristmas, Flatiron) {
