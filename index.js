430457fe5b261ac097f3e4e8308e42db9fc68ef7
Felix Kariuki <fkariuki1@live.co.uk>
2019-07-28 14:26:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(Flatiron) {
 }
 
 function happyHolidaysTo(MerryChristmas, Flatiron) {
-  return 'Happy ${Merry Christmas}, $"{Flatiron}'
+  return 'Happy ${Merry Christmas}, ${Flatiron}'
 }
 
 function holidayCountdown(thirty, Christmas) {
