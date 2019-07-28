192eec23ddc0ab47cf313adab28a5ab56f771efa
Felix Kariuki <fkariuki1@live.co.uk>
2019-07-28 14:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidayTo(Easter, Flatiron) {
   return `Happy ${Easter}, ${Flatiron}!`
 }
 
-function holidayCountdown(Christmas, 20) {
+function holidayCountdown(Christmas, days) {
   return `it's ${20} days until ${Christmas}!`
 }
